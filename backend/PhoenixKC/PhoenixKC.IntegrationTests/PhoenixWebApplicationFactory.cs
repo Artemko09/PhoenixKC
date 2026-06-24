9e062baede72e565d907c64b0d4005278489c736
tissle-main <its.main.ua@outlook.com>
2026-06-24 18:36:35 +0300

    chore/12: add unit and integration test projects

---
+++ backend/PhoenixKC/PhoenixKC.IntegrationTests/PhoenixWebApplicationFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace PhoenixKC.IntegrationTests;
+
+public sealed class PhoenixWebApplicationFactory : WebApplicationFactory<Program>
+{
+    #region Base
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            
+        });
+    }
+    #endregion
+}
