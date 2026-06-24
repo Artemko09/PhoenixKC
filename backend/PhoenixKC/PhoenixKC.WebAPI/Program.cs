457a551e216b657b8660d3ebba9fb3a9f3307917
tissle-main <its.main.ua@outlook.com>
2026-06-24 17:31:18 +0300

    chore/10: add 'Infrastructure' project with EF Core and Identity

--- backend/PhoenixKC/PhoenixKC.WebAPI/Program.cs
+++ backend/PhoenixKC/PhoenixKC.WebAPI/Program.cs
@@ -1,23 +1,33 @@
+using PhoenixKC.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<PhoenixDbContext>(options =>
+{
+    if(builder.Configuration.GetConnectionString("DefaultConnection") is not string connection_str)
+    {
+        throw new NullReferenceException("Connection string not found");
+    }
+    options.UseSqlServer(connection_str, builder =>
+    {
+        builder.MigrationsAssembly(typeof(PhoenixDbContext).Assembly.GetName().Name);
+    });
+});
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if(app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
+using(IServiceScope scope = app.Services.CreateScope())
+{
+    Console.WriteLine("Applying migrations...");
+    await scope.ServiceProvider.GetRequiredService<PhoenixDbContext>().Database.MigrateAsync();
+    Console.WriteLine("Migrations applied");
+}
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
