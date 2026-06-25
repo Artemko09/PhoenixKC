9c265a7f87e496f1be8212429503af45e17f4f18
tissle-main <its.main.ua@outlook.com>
2026-06-25 20:04:01 +0300

    chore/12: change test to use IMediator

--- backend/PhoenixKC/PhoenixKC.IntegrationTests/Features/Health/GetAllHealthTests.cs
+++ backend/PhoenixKC/PhoenixKC.IntegrationTests/Features/Health/GetAllHealthTests.cs
@@ -6,39 +6,36 @@ using PhoenixKC.WebAPI.Features.Health;
 using PhoenixKC.WebAPI.Features.Health.Dtos;
 using Microsoft.Extensions.DependencyInjection;
 using PhoenixKC.Infrastructure.Features.Health;
+using Mediator;
+using PhoenixKC.WebAPI.Features.Health.Handlers;
 
 namespace PhoenixKC.IntegrationTests.Features.Health;
 
 public sealed class GetAllHealthTests(PhoenixWebApplicationFactory thisFactory) : IClassFixture<PhoenixWebApplicationFactory>
 {
-    private HttpClient HttpClient { get; } = thisFactory.CreateClient();
-
     [Fact]
     public async Task GetAllHealth_ShouldSucceded_WhenHealthIsEmpty()
     {
         //Arrange
+        using IServiceScope handler_scope = thisFactory.Services.CreateScope();
+        IMediator mediator = handler_scope.ServiceProvider.GetRequiredService<IMediator>();
         HealthEntity entity = new()
         {
             Name = "TestHealth"
         };
-        using(IServiceScope scope = thisFactory.Services.CreateScope())
+        using(IServiceScope db_scope = thisFactory.Services.CreateScope())
         {
-            PhoenixDbContext db_context = scope.ServiceProvider.GetRequiredService<PhoenixDbContext>();
+            PhoenixDbContext db_context = db_scope.ServiceProvider.GetRequiredService<PhoenixDbContext>();
             await db_context.Health.AddAsync(entity, CancellationToken.None);
             await db_context.SaveChangesAsync(CancellationToken.None);
         }
         HealthDto dto = entity.ToDto();
 
         //Act
-        using HttpResponseMessage response = await HttpClient.GetAsync("/health", CancellationToken.None);
+        Result<IEnumerable<HealthDto>> result = await mediator.Send(new GetAllHealthQuery(), CancellationToken.None);
 
         //Assert
-        response.IsSuccessStatusCode.Should().BeTrue();
-        Result<HealthDto[]>? result = await response.Content.ReadFromJsonAsync<Result<HealthDto[]>>(CancellationToken.None);
-        if(result is null)
-        {
-            Assert.Fail("Failed to deserialize result of http response");
-        }
+        result.IsSuccess.Should().BeTrue();
         result.Value.Should().BeEquivalentTo([dto]);
     }
 }
